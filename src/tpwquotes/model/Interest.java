683430883cb0447cede3faaaf4a342ac97e67a29
cassiomms <cassio.matos.100.90@gmail.com>
2011-06-29 01:19:48 -0300

    beans interest e user

---
+++ src/tpwquotes/model/Interest.java
@@ -0,0 +1,26 @@
+package tpwquotes.model;
+
+import java.util.*;
+import java.io.Serializable;
+import javax.persistence.*;
+
+//Juros
+
+@Entity
+@Table(name="tInterest")
+public class Interest implements java.io.Serializable
+{
+	@Id
+	@Column(name="date", unique=true, nullable=false)
+	private Date date;
+	
+	@Column(name="value", nullable=false)
+	private Float value;
+	
+	public setDate(Date date) { this.date = date; }
+	public setValue(Float value) { this.value = value; }
+	
+	public Date getDate() { return this.date; }
+	public Float getValue() { return this.value; }
+	
+}
---
+++ src/tpwquotes/model/User.java
@@ -0,0 +1,56 @@
+package tpwquotes.model;
+
+import java.util.*;
+import java.io.Serializable;
+import javax.persistence.*;
+
+@Entity
+@Table(name="tUser")
+public class User implements java.io.Serializable
+{
+	@Id
+	@Column(name="cpf", unique=true, nullable=false)
+	private String cpf;
+	
+	@Column(name="name", nullable=false)
+	private String name;
+	
+	@Column(name="email", nullable=false)
+	private String email;
+	
+	@Column(name="password", nullable=false)
+	private String password;
+	
+	@Column(name="photo", nullable=true)
+	private String photo;
+	
+	@Column(name="phone", nullable=true)
+	private String phone;
+	
+	public setCpf(String cpf) { this.cpf = cpf; }
+	public setName(String name) { this.name = name; }
+	public setEmail(String email) { this.email = email; }
+	public setPassword(String password) { this.password = password; }
+	public setPhoto(String photo) { this.photo = photo; }
+	public setPhone(String phone) { this.phone = phone; }
+	
+	public String getCpf() { return this.cpf; }
+	public String getName() { return this.name; }
+	public String getEmail() { return this.email; }
+	public String getPassword() { return this.password; }
+	public String getPhoto() { return this.photo; }
+	public String getPhone() { return this.phone; }
+	
+	@OneToMany
+	@JoinColumn (name="cpf")
+	private Set<Transaction> transactions = new HashSet<Transaction>();
+	public Set<Transaction> getTransactions() { return this.transactions; }	
+	
+	@OneToMany
+	@JoinColumn (name="cpf")
+	private Set<Balance> balances = new HashSet<Balance>();
+	public Set<Balance> getBalances() { return this.balances; }
+	
+	
+	
+}
