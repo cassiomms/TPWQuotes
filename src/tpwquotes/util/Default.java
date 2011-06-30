70ad49bb8d401618495f7126301790bf21e1987f
Cassio Matos <cassio.matos.100.90@gmail.com>
2011-06-30 18:21:26 -0300

    update

--- src/tpwquotes/util/Default.java
+++ src/tpwquotes/util/Default.java
@@ -5,7 +5,7 @@ public interface Default {
 	// directory to store data
 	public String filePath = "/tmp/tpwquotes";
 
-	// servlets
+	// servlets - type parameter
 	public int LOGIN = 0;
 	public int LOGOUT = 1;
 	public int SIGNUP = 2;
