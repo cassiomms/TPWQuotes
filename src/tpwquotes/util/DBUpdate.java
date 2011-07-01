ce580d073df92dcd418ed8169750a258c9ea309d
Gabriel <tioshadow@gmail.com>
2011-07-01 00:08:00 -0300

    dbupdate novo

--- src/tpwquotes/util/DBUpdate.java
+++ src/tpwquotes/util/DBUpdate.java
@@ -10,25 +10,43 @@ import org.hibernate.criterion.*;
 public class DBUpdate {
 	
 	public static void GetUpdates() throws Exception {
+		Calendar cal = Calendar.getInstance();
 		Session dbsession = DB.getSessionFactory().getCurrentSession();
 		dbsession.beginTransaction();
 		
-		List qt = dbsession.createCriteria(Quote.class).list();
+		List list = dbsession.createCriteria(Quote.class).list();
 		
 		
-		ArrayList<Quote> quotes = new ArrayList(qt);
+		ArrayList<Quote> quotes = new ArrayList(list);
 		
-		ArrayList<Tick> ticks = QuoteBot.getTicks(quotes);
+		ArrayList<Tick> ticks = QuoteBot.getTicks(quotes,cal);
 
 		for(int i = 0; i<ticks.size(); i++)
 			dbsession.save(ticks.get(i));
+		
+		list = dbsession.createCriteria(User.class).list();
+		ArrayList<User> users = new ArrayList(list);
+   			
+		for(int i = 0; i<users.size(); i++)
+			{
+			User u = users.get(i);
+			Balance b = new Balance();
+			BalancePk bpk = new BalancePk();
+			bpk.setUser(u);
+			bpk.setDate(cal.getTime()); 
+			b.setId(bpk);
+			b.setFund(u.getFunds());
+			b.setDebt(u.getDebt());
+			dbsession.save(b);
+			}
+		
 		
 		dbsession.getTransaction().commit();
 		DB.getSessionFactory().close();
 	}
 
 
-	public static void main(String args[]) throws Exception {
+	public static void unitTest() throws Exception {
 	
 		Session dbsession = DB.getSessionFactory().getCurrentSession();
 		Quote q = new Quote();
